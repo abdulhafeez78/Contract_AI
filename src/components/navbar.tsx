e2ab2c4521a13f8bf9e151c6ab268c7b98aa659f
abdulhafeez78 <abdul.hafeez@kryptomind.com>
2024-06-28 23:56:24 +0500

    Fix: logo.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -35,7 +35,7 @@ export const NavBar = () => {
     <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="/">
-          <img className="logo-css" src="/assets/img/new_logo.png" alt="Logo" />
+          <img className="logo-css" src="/new_logo.png" alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
